c203a642c0fa02bf298c8bac2eaa3cc5f82c7eef
Kevin Bond <kevinbond@gmail.com>
2024-03-21 13:03:37 -0400

    feat: allow using spatie/image v3 w/o imagick

--- src/Image/Transformer/SpatieImageTransformer.php
+++ src/Image/Transformer/SpatieImageTransformer.php
@@ -11,6 +11,7 @@
 
 namespace Zenstruck\Image\Transformer;
 
+use Spatie\Image\Enums\ImageDriver;
 use Spatie\Image\Image;
 
 /**
@@ -24,7 +25,14 @@ final class SpatieImageTransformer extends FileTransformer
 {
     protected function object(\SplFileInfo $image): object
     {
-        return Image::load($image);
+        if (!(new \ReflectionMethod(Image::class, 'useImageDriver'))->isStatic()) {
+            // using spatie/image v2
+            return Image::load($image->getPathname());
+        }
+
+        return Image::useImageDriver(\class_exists(\Imagick::class) ? ImageDriver::Imagick : ImageDriver::Gd)
+            ->loadFile($image)
+        ;
     }
 
     protected static function expectedClass(): string
